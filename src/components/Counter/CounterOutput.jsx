a9c1c163cbfc66980b92e03166bd0bb39f27301a
AhmadShakeb Ahmadzai <shakeb.ahmadzai@mof.gov.af>
2025-07-16 16:17:51 +0430

    Initial commit

---
+++ src/components/Counter/CounterOutput.jsx
@@ -0,0 +1,11 @@
+import { log } from "../../log";
+
+export default function CounterOutput({ value }) {
+    log('<CounterOutput /> rendered', 2);
+
+    const cssClass = value >= 0 ? 'counter-output' : 'counter-output negative';
+
+    return (
+        <span className={cssClass}>{value}</span>
+    );
+};
